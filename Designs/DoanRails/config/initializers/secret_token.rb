81e3079da47864db7d105e44f2c4670d9edde154
nhannguyen <nguyennhan1303@gmail.com>
2013-04-12 09:39:13 +0700

    +Them project Rails vao thu muc designs

---
+++ Designs/DoanRails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoanRails::Application.config.secret_token = 'cdb6ce69c571a9552d1de604593d9f77247eb2d7c9798ecbdd0b02d7e6d10fb626765743c427745ebd1a1be4acbbb1db1077dc907cfc0e34b5ca773612b44b28'
